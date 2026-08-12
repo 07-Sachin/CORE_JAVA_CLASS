2b380bad68ca51cd539568e44c4ab6b4295d05f5
07-Sachin <2201020611@cgu-odisha.ac.in>
2026-08-12 21:31:35 +0530

    operators

---
+++ Bitwise_operator.java
@@ -0,0 +1,16 @@
+class Bitwise_operator{
+  public static void main(String[]args){
+   int a= 10;
+   int b=5;
+   int c= a&b;
+   System.out.println("AND : "+c);
+   int d= a|b;
+   System.out.println("OR : "+d);
+   int e =a^b;
+   System.out.println("XOR : "+e);
+   int f = a<<b;
+   System.out.println("LEFTSHIFT : "+f);
+   int g = a>>b;
+   System.out.println("RIGHTSHIFT : "+g);
+  }
+}
